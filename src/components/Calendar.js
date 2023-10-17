53de5d3adea989dab46cf9358f38872ddc066be1
Hugo Cullinan <hugo.cullinan@gmail.com>
2023-10-17 18:59:17 +1100

    implement add activity feature

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,39 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { Calendar as BigCalendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment/moment";
 
 const localizer = momentLocalizer(moment);
 
-const generateEvents = (month, year) => {
-  const numDaysInMonth = moment(`${year}-${month}`, "YYYY-M").daysInMonth();
-  const events = [];
-
-  for (let day = 1; day <= numDaysInMonth; day++) {
-    const startDate = moment(`${year}-${month}-${day} 00:00`, "YYYY-M-D HH:mm");
-    const endDate = moment(`${year}-${month}-${day} 23:59`, "YYYY-M-D HH:mm");
-
-    events.push({
-      title: "Running",
-      start: startDate.toDate(),
-      end: endDate.toDate(),
-    });
-  }
-
-  return events;
-};
+const addActivityToEntireMonth = (startDate, endDate, title) => ({
+  title: title,
+  start: startDate.toDate(),
+  end: endDate.toDate(),
+});
 
 const Calendar = () => {
   const today = moment();
-
   const year = today.year();
   const month = today.month() + 1;
 
-  const events = generateEvents(month, year);
+  const [activities, setActivities] = useState([]);
+  const [newActivityTitle, setNewActivityTitle] = useState("");
+
+  const handleAddActivity = () => {
+    if (newActivityTitle.trim() !== "") {
+      const numDaysInMonth = moment(`${year}-${month}`, "YYYY-M").daysInMonth();
+      const newActivities = [];
+
+      for (let day = 1; day <= numDaysInMonth; day++) {
+        const startDate = moment(
+          `${year}-${month}-${day} 00:00`,
+          "YYYY-M-D HH:mm"
+        );
+        const endDate = moment(
+          `${year}-${month}-${day} 23:59`,
+          "YYYY-M-D HH:mm"
+        );
+        newActivities.push(
+          addActivityToEntireMonth(startDate, endDate, newActivityTitle)
+        );
+      }
+
+      setActivities([...activities, ...newActivities]);
+      setNewActivityTitle("");
+    }
+  };
+
   return (
     <>
+      <div>
+        <input
+          type="text"
+          onChange={(event) => setNewActivityTitle(event.target.value)}
+          placeholder="Enter Activity"
+        />
+        <button onClick={handleAddActivity}>Add</button>
+      </div>
       <BigCalendar
         localizer={localizer}
-        events={events}
+        events={activities}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 500 }}
