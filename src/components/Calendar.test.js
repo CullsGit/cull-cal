723971373e3ffef22b5fcb9292102c992e0e8ffa
Hugo Cullinan <hugo.cullinan@gmail.com>
2023-10-18 21:50:42 +1100

    add test to simulate adding an activity to calendar

--- src/components/Calendar.test.js
+++ src/components/Calendar.test.js
@@ -4,9 +4,10 @@ import "@testing-library/jest-dom/extend-expect";
 import { render, screen, fireEvent } from "@testing-library/react";
 import Calendar from "./Calendar";
 
+const today = moment();
+
 it("renders Calendar component correctly", () => {
   render(<Calendar />);
-  const today = moment();
   const expectedMonthYear = today.format("MMMM YYYY");
 
   const inputElement = screen.getByPlaceholderText("Enter Activity");
@@ -16,4 +17,20 @@ it("renders Calendar component correctly", () => {
   expect(inputElement).toBeInTheDocument();
   expect(addButton).toBeInTheDocument();
   expect(monthHeader).toBeInTheDocument();
+});
+
+it("successfully adds an activity to every day of the month after user input", () => {
+  render(<Calendar />);
+
+  const daysInCurrentMonth = today.daysInMonth();
+
+  const inputElement = screen.getByPlaceholderText("Enter Activity");
+  const addButton = screen.getByText("Add");
+
+  fireEvent.change(inputElement, { target: { value: "Read" } });
+  fireEvent.click(addButton);
+
+  const addedActivities = screen.getAllByText("Read");
+
+  expect(addedActivities.length).toBe(daysInCurrentMonth);
 });
